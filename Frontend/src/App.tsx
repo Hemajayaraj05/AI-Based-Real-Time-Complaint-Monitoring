7bd5825a9739b32b113c2b5bf471366c44ce8055
Abinaya S <abinayas495100@gmail.com>
2026-01-03 11:23:56 +0530

    Add frontend UI updates

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import AssignedIssues from "./pages/AssignedIssues";
 import MainLogin from "./pages/MainLogin";
+import RaiseComplaint from "./pages/RaiseComplaint";
 
 function App() {
-  return <MainLogin />;
+  // return <MainLogin />;
+  // return <RaiseComplaint/>;
+  return <AssignedIssues/>
 }
 
 export default App;
